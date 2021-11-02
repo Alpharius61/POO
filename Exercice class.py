64c14fd97371dcbe6d9a362db89dce9e33cb6203
Alpharius61 <monlouis.gael@gmail.com>
2021-11-02 20:14:14 +0100

    EX1 Ok

---
+++ Exercice class.py
@@ -0,0 +1,18 @@
+#  coding in utf-8
+
+
+from Animals import Animal
+
+def main ():
+    InputRight = False
+    while InputRight == False :
+        try :
+            AnimalChoice = input("Choisir un animal :")
+        
+        except:
+            continue
+        
+
+
+if __name__ == "__main__" :
+    main()
